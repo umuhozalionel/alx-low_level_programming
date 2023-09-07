7497a490bce233133e53190baeb7fb2802d8265d
umuhozalionel <muhozalionel@gmail.com>
2023-09-07 16:47:00 -0700

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#inude <stdio.h>
+/**
+ * main - maim funcyion where everything supposed to take place
+ *
+ * return : always 0
+ *
+ */
+int main (void)
+{
+	put s ("\" programming is like building a multilangural puzzle");
+	return (0);
+}

5cf3f7ae6809c4c4d6dcdf55c72c19066fb1547b
umuhozalionel <muhozalionel@gmail.com>
2023-09-07 17:12:39 -0700

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - maim funcyion where everything supposed to take place
+ * main - main funcyion where everything supposed to take place
  *
  * return: always 0
  *
  */
 int main(void)
 {
-	puts("\" programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }

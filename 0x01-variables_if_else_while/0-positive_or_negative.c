7b19417fb1ffffd50dc75d86065a96cf5be18fbd
umuhozalionel <muhozalionel@gmail.com>
2023-09-09 06:17:26 -0700

    negative and positive in while conditions today

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("%d is positif", n);
+		printf("%d is positive", n);
 	else if (n == 0)
 		printf("%d is zero", n);
 	else

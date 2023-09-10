863d7225b19eeeaeaead34e0c56cc6c5362f522f
umuhozalionel <muhozalionel@gmail.com>
2023-09-10 01:00:06 -0700

    Various format with alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- *  main - program that prints the alphabet in lowercase and upper case
+ * main - program that prints the alphabet in lowercase and upper case
  *
- *  Return: Always 0
+ * Return: Always 0
  */
 
 int main(void)
 {
         char l;
-
-        for (l = 'a' ; l <= 'z' ; l++)
+	
+	for (l = 'a' ; l <= 'z' ; l++)
 	       	putchar(l);
-
-        for (l = 'A' ; l <= 'Z' ; l++)
+	for (l = 'A' ; l <= 'Z' ; l++)
 		putchar(l);
 	putchar('\n');
 

a58f9d6416a21d3b7ec99016532b9cfc42862dcb
umuhozalionel <muhozalionel@gmail.com>
2023-09-10 00:51:16 -0700

    Various format with alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,11 @@ int main(void)
         char l;
 
         for (l = 'a'; l <= 'z'; l ++)
-        putchar(l);
+	       	putchar(l);
 
         for (l = 'A'; l <= 'Z'; l ++)
-        putchar(l);
-
-        putchar('\n');
+		putchar(l);
+	putchar('\n');
 
         return (0);
 } 

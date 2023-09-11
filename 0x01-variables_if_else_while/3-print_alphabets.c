bbd6e2364db5fd3adc54093480be03efeb9b7f78
umuhozalionel <muhozalionel@gmail.com>
2023-09-10 23:56:48 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,16 +3,19 @@
 /**
  * main - main function
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 
 int main(void)
 {
        	char l;
+
 	for (l = 'a'; l <= 'z'; l++)
                 putchar(l);
+
         for (l = 'A'; l <= 'Z'; l++)
-                putchar(l)
+                putchar(l);
+			
 			putchar('\n');
         return (0);
 }

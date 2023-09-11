80f102b6b03a101dbfd75677fd99506eab95ff59
umuhozalionel <muhozalionel@gmail.com>
2023-09-10 23:51:57 -0700

    running alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,11 @@
 
 int main(void)
 {
-        char l;
-
-        for (l = 'a' ; l <= 'z' ; l++)
+       	char l;
+	for (l = 'a'; l <= 'z'; l++)
                 putchar(l);
-        for (l = 'A' ; l <= 'Z' ; l++)
-                putchar(l);
-        putchar('\n');
+        for (l = 'A'; l <= 'Z'; l++)
+                putchar(l)
+			putchar('\n');
         return (0);
 }

914fbe61ec76e02e41cd52fdcd927f6679c65b53
umuhozalionel <muhozalionel@gmail.com>
2023-09-10 23:05:32 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	char l;
+        char l;
 
-	for (l = 'a' ; l <= 'z' ; l++)
-		putchar(l);
-	for (l = 'A' ; l <= 'Z' ; l++)
-		putchar(l);
-	putchar('\n');
+        for (l = 'a' ; l <= 'z' ; l++)
+                putchar(l);
+        for (l = 'A' ; l <= 'Z' ; l++)
+                putchar(l);
+        putchar('\n');
         return (0);
 }

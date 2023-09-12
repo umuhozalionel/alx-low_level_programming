c6d93784b6bc2bfcc0995404d92d7c401861a5d3
umuhozalionel <muhozalionel@gmail.com>
2023-09-12 09:07:24 -0700

    Fibonacci sequence whose values do not exceed 4,000,000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,26 +2,24 @@
 
 
 /**
-  * main - the main printing funtion
-  * less than 4000000.
-  * Return: 0.
+  * main - main function 
+  * Return: 0
   */
-
-
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
+	int limit = 4000000;
+	int term1 = 1;
+	int term2 = 2;
+	int next;
+	int sum = 2;
 
-
-	while (k + j < 4000000)
+	while (next <= limit)
 	{
-		k += j;
-		if (k % 2 == 0)
-		sum += k;
-		j = k - j;
-		++i;
+		next = term1 + term2;
+		if (next % 2 == 0)
+			sum = sum + next;
+		term1 = term2;
+		term2 = next;
 	}
-	printf("%ld", sum);
 	return (0);
 }

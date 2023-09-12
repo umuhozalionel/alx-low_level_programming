0a6ab39aede579b2ab11995481d1421f39ebca60
umuhozalionel <muhozalionel@gmail.com>
2023-09-12 07:16:38 -0700

    computing the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,33 +2,18 @@
 
 
 /**
-  * main - check the code.
-  *
-  * Return: Always 0.
+  *_abs - checking the code.
+  *@r: function parameter
+  * Return: -r or r
   */
-int main(void)
+int _abs(int r)
 {
-	int r;
+	if (r < 0)
+		return (-r);
+	else if (r >= 0)
+	{
+		return (r);
+	}
 
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
 	return (0);
 }

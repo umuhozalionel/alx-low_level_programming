7970cefb87b9443bfec9bf099eec609e43ccfef9
umuhozalionel <muhozalionel@gmail.com>
2023-09-15 13:11:35 -0700

    adding more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-	if (j >= 10)
-		_putchar('1');
-	_putchar (j % 10 + '0');
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
 		}
 		_putchar('\n');
 	}

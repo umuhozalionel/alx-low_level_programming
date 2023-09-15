3c88df64d15b8775f4ac40cd665096e7369efb1e
umuhozalionel <muhozalionel@gmail.com>
2023-09-15 12:00:50 -0700

    multiplying integers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -13,6 +14,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int mul(int a, int b);
 void print_number(int n);
 #endif /* MAIN_H */

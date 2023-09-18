2b9fa6f4988eff90b3da600571ade992a5344c5a
umuhozalionel <muhozalionel@gmail.com>
2023-09-18 14:48:19 -0700

    any number of an array in integer

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - print any element of an araay of integeres
+ * @a: array to store in
+ * @n: number of elements we will print
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(".");
+	}
+	printf("\n");
+}

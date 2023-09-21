3ed5eb69e0336eae9e68a3f5ef521eabb9ba6e0c
umuhozalionel <muhozalionel@gmail.com>
2023-09-21 10:27:56 -0700

    encoding strings into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -18,7 +18,7 @@ char *leet(char *c)
 		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
 			/*32 is the difference between lowercase letters and uppercase letters*/
-			if (*c == key[i] + 32)
+			if (*c == key[i] || *c == key[i] + 32)
 			{
 				*c = 48 + value[i];
 			}

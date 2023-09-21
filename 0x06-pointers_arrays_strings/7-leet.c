fe9e8ef998efb065a7588d60790761579635e877
umuhozalionel <muhozalionel@gmail.com>
2023-09-21 10:23:56 -0700

    mozart music in 1377

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encoding a string into 1337
+ * @c: string
+ * Return: string thats encoded
+ */
+
+char *leet(char *c)
+{
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			/*32 is the difference between lowercase letters and uppercase letters*/
+			if (*c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+	return (cp);
+}

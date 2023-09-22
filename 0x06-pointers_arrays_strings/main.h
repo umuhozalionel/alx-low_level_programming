6cb04c407b1acd5eb5c054d5751a6c8483b82f28
umuhozalionel <muhozalionel@gmail.com>
2023-09-21 18:16:57 -0700

    2 numbers with ascii

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,6 +17,10 @@ void print_number(int n);
 int _putchar(char c);
 int main(void);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void printHexes (char *b, int start, int end);
+int isPrintableASCII(int n);
+void printASCII(char *b, int start, int end);
+void rev_string(char *n);
 void print_buffer(char *b, int size);
 
 #endif

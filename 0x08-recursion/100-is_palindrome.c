e27593accf4f3e9f2be30ba2aa16c0deb90a9e3b
umuhozalionel <muhozalionel@gmail.com>
2023-09-27 19:01:26 -0700

    checking if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -39,7 +39,7 @@ int _strlen_recursion(char *s)
  */
 int check_pal(char *s, int i, int len)
 {
-	if (*(s + 1) != *(s + len - 1))
+	if (*(s + i) != *(s + len - 1))
 		return (0);
 	if (i >= len)
 		return (1);

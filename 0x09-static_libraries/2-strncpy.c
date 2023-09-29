18178a0479f6b3bb9cb7cd48152fa6ca292a3fec
umuhozalionel <muhozalionel@gmail.com>
2023-09-29 05:45:04 -0700

    My first static library file created

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy strings
+ * @dest: destination of strings
+ * @src: source of the string
+ * @n: number of bytes to copy
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+  *_abs - checking the code.
+  *@r: function parameter
+  * Return: -r or r
+  */
+int _abs(int r)
+{
+	if (r < 0)
+		return (-r);
+	else if (r >= 0)
+	{
+		return (r);
+	}
+
+	return (0);
+}

b74b4d398fff087d2baa4408431e183edb1907a6
umuhozalionel <muhozalionel@gmail.com>
2023-10-19 16:09:49 -0700

    new node at the end\gitpush

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to put in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *n;
+	list_t *t = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	n = malloc(sizeof(list_t));
+	if (!n)
+		return (NULL);
+
+	n->str = strdup(str);
+	n->len = len;
+	n->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = n;
+		return (n);
+	}
+	while (t->next)
+		t = t->next;
+	t->next = n;
+	return (n);
+}
+

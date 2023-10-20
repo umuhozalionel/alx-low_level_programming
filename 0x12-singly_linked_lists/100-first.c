9a40652c499b8fdc90e7b9c2321cb823c9cd2c4e
umuhozalionel <muhozalionel@gmail.com>
2023-10-19 17:45:04 -0700

    before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include "lists.h"
 
 /**
- * first - before main function executed
+ * first - Main function is executed
  */
 
 void first(void) __attribute__ ((constructor));
-void before_main(void)
+void first(void)
 {
-	printf("You're beat!, you must allow,\n");
-	printf("i bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }

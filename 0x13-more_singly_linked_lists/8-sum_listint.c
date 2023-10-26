8199b884aaf314add4395c1bd31fbf5cc68fcf67
umuhozalionel <muhozalionel@gmail.com>
2023-10-25 17:34:31 -0700

    Returning the sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Function that sum lists
+ * @head: head nodo
+ *
+ * Return: sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	for (sum = 0; head; sum += head->n, head = head->next)
+		;
+	return (sum);
+}

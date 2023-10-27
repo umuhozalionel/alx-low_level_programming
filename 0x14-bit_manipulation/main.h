38e9c287be7df24747317a6212e205faf49681e3
umuhozalionel <muhozalionel@gmail.com>
2023-10-26 19:06:51 -0700

    calculating base powers

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+unsigned long int _pow(unsigned int base, unsigned int power);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);

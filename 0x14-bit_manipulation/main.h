d83d6e7d0aaa7ebb766a723473caf5fe9731c672
umuhozalionel <muhozalionel@gmail.com>
2023-10-26 20:29:53 -0700

    flip bits

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 unsigned long int _pow(unsigned int base, unsigned int power);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
